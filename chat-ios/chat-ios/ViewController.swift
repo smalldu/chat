0c0edf8d6119e0c934bcffcd98d3879809755047
duzhe <363958265@qq.com>
2018-08-11 17:57:40 +0800

    update

--- chat-ios/chat-ios/ViewController.swift
+++ chat-ios/chat-ios/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import SocketIO
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
+  @IBOutlet weak var valueField: UITextField!
+  
+  var manager: SocketManager!
+  var socketIOClient: SocketIOClient!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    initToSocket()
+  }
+  
+  func initToSocket() {
+    
+    manager = SocketManager(socketURL: URL(string: "http://localhost:5000")!, config: [.log(true), .compress])
+    socketIOClient = manager.defaultSocket
+    
+    socketIOClient.on(clientEvent: .connect) {data, ack in
+      print(data)
+      print("已连接")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    socketIOClient.on(clientEvent: .error) { (data, eck) in
+      print(data)
+      print("发生错误")
     }
-
-
+    
+    socketIOClient.on(clientEvent: .disconnect) { (data, eck) in
+      print(data)
+      print("已断开")
+    }
+    
+    socketIOClient.on(clientEvent: .reconnect) { (data, eck) in
+      print(data)
+      print("重新连接")
+    }
+    
+    
+  }
+  
+  @IBAction func sendMsg(_ sender: Any) {
+    valueField.resignFirstResponder()
+    let value = valueField.text ?? ""
+    socketIOClient.emit("client_event",["data":value])
+  }
+  
+  @IBAction func connectSocket(_ sender: Any) {
+    socketIOClient.connect()
+  }
+  
+  @IBAction func dissconnect(_ sender: Any) {
+    socketIOClient.disconnect()
+  }
 }
+
 
--- chat/manager.py
+++ chat/manager.py
@@ -4,28 +4,27 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
-socketio =  SocketIO(app, async_mode='eventlet')
-
+socketio = SocketIO(app, async_mode='eventlet')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('my event')
+def test_message(message):
+    emit('my response', {'data': message['data']})
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
+@socketio.on('my broadcast event')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
 
-@socketio.on('client_event')
-def client_msg(msg):
-    print('msg client = {}'.format(msg))
-    emit('server_response', {'data': msg['data']})
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
 
-
-@socketio.on('connect_event')
-def connected_msg(msg):
-    print('msg = {}'.format(msg))
-    emit('server_response', {'data': msg['data']})
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
 
 
 if __name__ == '__main__':
